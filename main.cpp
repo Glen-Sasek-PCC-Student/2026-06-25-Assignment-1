feaa390135e9ec02f63a3dd7e054d3419299bfcd
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-06-26 02:47:51 +0000

    Design done

--- main.cpp
+++ main.cpp
@@ -26,29 +26,84 @@ int main() {
 // ------------- DESIGN -------------
 /* 
 Program Name:
+Tax Calculator
 
 Program Description:
+During September, businesses across the United States take time to thank their payroll teams and reflect on all of the successful paydays of the past year. While technology has made it easier than ever for HR teams to pay their employees, crunching the numbers can still be a laborious task for payroll professionals. National Payroll Week serves as a reminder that there are dedicated professionals working hard everyday to ensure we get paid accurately and on time.
 
 Design:
 A. INPUT
-Define the input variables including name data type. 
+int employeeID = 0;
+int employeeHours = 0;
+int USDPerHour = 0;
+int integerWithholdingRate = 0;
 
 B. OUTPUT
-Define the output variables including data types. 
+double grossPayUSD = 0.0;
+double federalTaxWitholdingUSD = 0.0;
+double netPayUSD = 0.0;
 
 C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
+First calculate the total gross pay from the hours worked and the hourly rate inputs. 
+grossPayUSD = employeeHours * USDPerHour;
+double decimalWitholdingRate = integerWithholdingRate / 100.0;
+federalTaxWitholdingUSD = grossPayUSD * decimalWitholdingRate;
+netPayUSD = grossPayUSD - federalTaxWitholdingUSD;
 
 D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+WELCOME "Welcome to my Weekly Payroll program!!"
+
+PROMPT "Enter your employee ID number (numbers only): "
+INPUT employeeID
+
+PROMPT "Enter number of hours worked (whole numbers): "
+INPUT employeeHours
+
+PROMPT "Enter the hourly rate: "
+INPUT USDPerHour
+
+PROMPT "Enter the federal withholding rate: "
+INPUT federalWithholdingRatePercent
+
+DO CALCULATIONS
+
+HEADING "Your Payroll Summary: "
+
+LABEL "Gross Pay: "
+DATA grossPayUSD
+
+LABEL "Federal Tax Withholding: "
+DATA federalTaxWitholdingUSD
+
+LABEL "Net Pay: "
+DATA netPayUSD
+
+GOODBYE "Thank you for using my Weekly Payroll program!!"
 
 
 SAMPLE RUNS
-Copy from assignment document.
+Welcome to my Weekly Payroll program!!
+Enter your employee ID number (numbers only): 34567
+Enter number of hours worked (whole numbers): 40
+Enter the hourly rate: 17
+Enter the federal withholding rate: 15
+Your Payroll Summary:
+Total Gross Pay: $680
+Federal Tax Withholding: $102
+Net Pay: $578
+Thank you for using my Weekly Payroll program!!
+
+
+Welcome to my Weekly Payroll program!!
+Enter your employee ID number (numbers only): 12345
+Enter number of hours worked (whole numbers): 30
+Enter the hourly rate: 22
+Enter the federal withholding rate: 10
+Your Payroll Summary:
+Total Gross Pay: $660
+Federal Tax Withholding: $66
+Net Pay: $594
+Thank you for using my Weekly Payroll program!!
+
 
 */
